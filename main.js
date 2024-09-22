a2c87ed56d91a70d783f2b99efd2dfbee6cf1184
Kevin Mullins <krmullins@gmail.com>
2024-09-21 20:48:49 -0700

    final pet card work completed

--- main.js
+++ main.js
@@ -1,52 +1,49 @@
-const template = document.querySelector("#pet-card-template");
-const wrapper = document.createDocumentFragment();
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
 
 async function start() {
-  const weatherPromise = await fetch(
-    "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
-  );
-  const weatherData = await weatherPromise.json();
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+  const weatherData = await weatherPromise.json()
 
-  const ourTemperature = weatherData.properties.periods[0].temperature;
-  document.querySelector("#temperature-output").textContent = ourTemperature;
+  const ourTemperature = weatherData.properties.periods[0].temperature
+  document.querySelector("#temperature-output").textContent = ourTemperature
+
 }
 
-start();
+start()
 
 async function petsArea() {
-  const petsPromise = await fetch(
-    "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
-  );
-  const petsData = await petsPromise.json();
-  petsData.forEach((pet) => {
-    const clone = template.content.cloneNode(true);
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsData = await petsPromise.json()
+  petsData.forEach(pet => {
+    const clone = template.content.cloneNode(true)
 
-    clone.querySelector("h3").textContent = pet.name;
-    clone.querySelector(".pet-description").textContent = pet.description;
-    clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear);
+    clone.querySelector(".pet-card").dataset.species = pet.species
 
-    if (!pet.photo) pet.photo = "images/fallback.jpg";
+    clone.querySelector("h3").textContent = pet.name
+    clone.querySelector(".pet-description").textContent = pet.description
+    clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
 
-    clone.querySelector(".pet-card-photo img").src = pet.photo;
-    clone.querySelector(
-      ".pet-card-photo img"
-    ).alt = `A ${pet.species} named ${pet.name}`;
+    if (!pet.photo) pet.photo = "images/fallback.jpg"
 
-    wrapper.appendChild(clone);
-  });
-  document.querySelector(".list-of-pets").appendChild(wrapper);
+    clone.querySelector(".pet-card-photo img").src = pet.photo
+    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}.`
+
+    wrapper.appendChild(clone)
+  })
+  document.querySelector(".list-of-pets").appendChild(wrapper)
 }
 
-petsArea();
+petsArea()
 
 function createAgeText(birthYear) {
-  const currentYear = new Date().getFullYear();
-  const age = currentYear - birthYear;
+  const currentYear = new Date().getFullYear()
+  const age = currentYear - birthYear
 
-  if (age == 1) return "1 year old";
-  if (age == 0) return "Less than a year old";
+  if (age == 1) return "1 year old"
+  if (age == 0) return "Less than a year old"
 
-  return `${age} years old`;
+  return `${age} years old`
 }
 
 // pet filter button code
@@ -57,13 +54,19 @@ allButtons.forEach(el => {
 })
 
 function handleButtonClick(e) {
-  //remove active class from any and all buttons
+  // remove active class from any and all buttons
   allButtons.forEach(el => el.classList.remove("active"))
 
   // add active class to the specific button that just got clicked
   e.target.classList.add("active")
 
-
-  //actually filter the pets down below
-
+  // actually filter the pets down below
+  const currentFilter = e.target.dataset.filter
+  document.querySelectorAll(".pet-card").forEach(el => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+      el.style.display = "grid"
+    } else {
+      el.style.display = "none"
+    }
+  })
 }
